d5a7425537eef01b68a48bf314048400e18c04f2
belhyun <belhyun@gmail.com>
2014-04-01 10:18:36 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RsWww::Application.config.secret_key_base = '2ccba781c5c62d2f5594974b4926836b6adf6f354c15e9562de8b7146276f8cd9df5616d39c5e07ccfe2329ba73d12fe7660ed40684cff2b35faf8ee9c19df16'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RsWww::Application.config.session_store :cookie_store, key: '_rs-www_session'
